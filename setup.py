56921717c0f972c03b24a3169512ec6350144934
Steven Hamblin <steven.hamblin@gmail.com>
2014-06-11 00:22:20 -0700

    Changing version number and development status.

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup, find_packages
 
 files = ["journal_files/*"]
 
+readme = open('README.md','r')
+README_TEXT = readme.read()
+readme.close()
+
 setup(name = "bibbreviate",
       version = "0.0.1",
       description = "Abbreviate journal titles in BibTex files.",
+      long_description = README_TEXT,
       author = "Steven Hamblin",
       author_email = "steven.hamblin@gmail.com",
       url = "https://github.com/Winawer/bibbreviate",
