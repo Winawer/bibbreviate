6abac38578304ba4d5521a0ed2825a3e35d130f1
Steven Hamblin <steven.hamblin@gmail.com>
2014-06-10 23:48:32 -0700

    Cleaning up setup.py

--- setup.py
+++ setup.py
@@ -10,16 +10,8 @@ setup(name = "bibbreviate",
       url = "https://github.com/Winawer/bibbreviate",
       packages = find_packages(exclude="tests"),
       package_data = {'src':['journal_files/*.txt']},
-      #include_package_data = True,
-      #package_data = {'':['*.txt']},
-      #packages = find_packages('src'),
-      #package_dir = {'': 'src'},
       install_requires = ['setuptools','bibtexparser'],
       entry_points = { 'console_scripts' : ['bibbrev = src.bibbrev:main'] },
-      #      entry_points = """
-      #[console_scripts]
-      #bibbreviate = bibbrev:main
-      #""",
       classifiers = [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
